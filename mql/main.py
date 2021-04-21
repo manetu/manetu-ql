a49a36337f5cbace104f3455e4f565358e740447
Alex Tsariounov <alext@manetu.com>
2021-04-21 17:59:27 -0600

    Reorganize argument parsers to nested.
    Signed-off-by: Alex Tsariounov <alext@manetu.com>

--- mql/main.py
+++ mql/main.py
@@ -11,18 +11,27 @@ for details and any restrictions.
 from mql.version import version
 import argparse, importlib, sys
 
-# global parser declarations
+# parser declarations for common and all subcommands
 parser = argparse.ArgumentParser(description=f'Manetu.io GraphQL interface, version {version}')
 
-parser.add_argument('command', action='store',
-                    help='run the specified commmand (schema, getall)')
-
+#  global options
 parser.add_argument('-v', '--verbose', action='count', default=0,
                     help='increase verbose output')
 parser.add_argument('-p', '--pat', action='store',
                     help='specify personal access token to use')
 parser.add_argument('-j', '--jwt', action='store',
                     help='specify jwt to use')
+
+#  the commands
+subparsers = parser.add_subparsers(help='commands', dest='command')
+
+#  schema command
+schema_parser = subparsers.add_parser('schema', help='get schema from server')
+
+#  getall command which gets all fields in an object
+getall_parser = subparsers.add_parser('getall', help='get all fields for an object')
+getall_parser.add_argument('object', action='store',
+                           help='the object of interest')
 
 # cmdline entry point and dispatcher
 def main():
