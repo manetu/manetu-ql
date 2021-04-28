539d9cf5a4f65c9d7fee3bd644d05a86a0e43b62
Alex Tsariounov <alext@manetu.com>
2021-04-28 16:29:59 -0600

    Add __version__ to module.
    Signed-off-by: Alex Tsariounov <alext@manetu.com>

--- mql/__init__.py
+++ mql/__init__.py
@@ -5,3 +5,5 @@ Author: Alex Tsariounov <alext@manetu.com>
 This is free software; please see the LICENSE file
 for details and any restrictions.
 """
+
+from mql.version import version as __version__
